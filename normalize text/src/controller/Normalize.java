d0c939ceec74ba4cd46a293c6c1b37b6996fd5f6
Phương <139949617+shaw2123@users.noreply.github.com>
2023-09-27 11:28:33 +0700

    Add files via upload

---
+++ normalize text/src/controller/Normalize.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import model.Text;
+
+
+public class Normalize {
+    
+    public static void readAndWrite(){
+        BufferedReader br = null;
+        try {
+            int countLine = Text.countLine();
+            int count = 1;
+            br = new BufferedReader(new FileReader(new File("input.txt")));
+            PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("output.txt", true)));
+            String line;
+            //write until end file
+            while ((line = br.readLine()) != null) {
+                //check line empty
+                if (Text.isLineEmpty(line)) {
+                    continue;
+                }
+                line = Text.formatOneSpace(line);
+                line = Text.formatSpecialCharacters(line);
+                line = Text.afterDotUpperCase(line);
+                line = Text.noSpaceQuotes(line);
+                line = Text.firstUpercase(line);
+                line = Text.lastAddDot(line);
+                pw.print(line);
+                if (count < countLine) {
+                    pw.print(System.getProperty("line.separator"));
+                }
+                count++;
+            }
+            br.close();
+            pw.close();
+            System.out.println("Normalize successful.");
+        } catch (FileNotFoundException ex) {
+            System.err.println("Can't not found");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    
+}
